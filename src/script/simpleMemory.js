b40080bb7e5d74cde9dcde45725739411d53d455
dummerchen <1179502349@qq.com>
2020-11-01 21:08:21 +0800

    删去@以便实时获取最新版本

--- src/script/simpleMemory.js
+++ src/script/simpleMemory.js
@@ -432,7 +432,7 @@ function init() {
 // get file url
 function getJsDelivrUrl(file, directory) {
     file = setFileNameMin(file, directory);
-    return 'https://cdn.jsdelivr.net/gh/'+(window.cnblogsConfig.GhUserName)+'/'+(window.cnblogsConfig.GhRepositories)+'@'+(window.cnblogsConfig.GhVersions)+'/' + (file ? file : '');
+    return 'https://cdn.jsdelivr.net/gh/'+(window.cnblogsConfig.GhUserName)+'/'+(window.cnblogsConfig.GhRepositories)+'/' + (file ? file : '');
 }
 
 // optimization file name
