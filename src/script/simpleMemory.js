45d7e12c1c443ba4b34338688e608076efd16556
dummerchen <1179502349@qq.com>
2020-11-02 14:02:55 +0800

    回到没有加master的状态..

--- src/script/simpleMemory.js
+++ src/script/simpleMemory.js
@@ -434,8 +434,8 @@ function getJsDelivrUrl(file, directory) {
     file = setFileNameMin(file, directory);
     // 好像还没@master好用
     // return 'https://cdn.jsdelivr.net/gh/'+(window.cnblogsConfig.GhUserName)+'/'+(window.cnblogsConfig.GhRepositories)+'/' + (file ? file : '');
-
-    return 'https://cdn.jsdelivr.net/gh/'+(window.cnblogsConfig.GhUserName)+'/'+(window.cnblogsConfig.GhRepositories)+'@'+window.cnblogsConfig.GhVersions+'/' + (file ? file : '');
+  //return 'https://cdn.jsdelivr.net/gh/'+(window.cnblogsConfig.GhUserName)+'/'+(window.cnblogsConfig.GhRepositories)+'@'+(window.cnblogsConfig.GhVersions)+'/' + (file ? file : '');
+    return 'https://cdn.jsdelivr.net/gh/'+(window.cnblogsConfig.GhUserName)+'/'+(window.cnblogsConfig.GhRepositories)+'@'+(window.cnblogsConfig.GhVersions)+'/' + (file ? file : '');
 }
 
 // optimization file name
